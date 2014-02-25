c754e7603221541cbffb90617cdc44bd3f1d8252
mber <mber@mbermacbook.local>
2014-02-25 14:56:58 -0500

    om-ify'd

--- project.clj
+++ project.clj
@@ -2,11 +2,46 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/tools.reader "0.8.2"]
+                 [compojure "1.1.6"]]
                  [korma "0.3.0-RC6"]
                  [mysql/mysql-connector-java "5.1.25"]
-                 [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler caucusrw.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-json "0.2.0"]
+                 [cljs-http "0.1.2"]
+                 [om "0.5.0"]
+                 [com.facebook/react "0.8.0.1"]
+
+
+  :plugins [[lein-cljsbuild "1.0.1"]
+            [lein-ring "0.8.10"]
+            [lein-pdo "0.1.1"]]
+
+  :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
+
+  :min-lein-version "2.0.0"
+  :uberjar-name "caucusrw-standalone.jar"
+
+  :ring {:handler caucusrw.handler/app
+         :init caucusrw.handler/init}
+
+  :source-paths ["src/clj"]
+
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/cljs"]
+                        :compiler {:output-to "resources/public/js/app.js"
+                                   :output-dir "resources/public/js/out"
+                                   :optimizations :none
+                                   :source-map true}}
+                       {:id "release"
+                        :source-paths ["src/cljs"]
+                        :compiler {:output-to "resources/public/js/app.js"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :preamble ["react/react.min.js"]
+                                   :externs ["react/externs/react.js"]}}]})
+
+
+
---
+++ src/clj/caucusrw/handler.clj
@@ -0,0 +1,22 @@
+(ns caucusrw.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [compojure.core :refer [GET defroutes]]
+            [ring.util.response :refer [resource-response response]]
+            [ring.middleware.json :as middleware]))
+
+
+(defroutes app-routes
+  (GET  "/" [] (resource-response "index.html" {:root "public"}))
+  (GET  "/widgets" [] (response [{:name "Widget 1"} {:name "Widget 2"}]))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+#_(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
